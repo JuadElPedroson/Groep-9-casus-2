9e395526ccbd221426c02f5c7240822f1296b68f
bayoums2345 <seif.bayoumi@hva.nl>
2026-09-23 14:00:53 +0200

    commit

--- pages/3_Databronnen.py
+++ pages/3_Databronnen.py
@@ -4,13 +4,12 @@ from data import load_data
 st.title("Databronnen")
 
 st.write(
-    "Twee bronnen zijn samengevoegd: de 500 meest gestreamde artiesten op "
-    "Spotify en een tabel met landengegevens zoals bevolking en GDP per "
-    "hoofd. Ze zijn gekoppeld op het land van herkomst van de artiest."
+    "we hebben tijdens deze casus AI vornamelijk gebruikt om data op te halen via de API van kaggel en als een ondersteunende tool voor onze coding skills "
 )
 
 st.write("""
 Countries of the World. (n.d.). Retrieved September 23, 2026, from https://www.kaggle.com/datasets/fernandol/countries-of-the-world
+
 Most Streamed Artists on Spotify. (n.d.). Retrieved September 23, 2026, from https://www.kaggle.com/datasets/rishavsvault/most-streamed-artists-on-spotify
 
 Streamlit • A faster way to build and share data apps. (2021, January 14). https://streamlit.io/
